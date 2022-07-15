d5e935680f93da2bd97bbd72e20219861c0b5b6e
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-07-14 22:05:54 -0700

    Implemented Binary Search Algorithm

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,14 +1,24 @@
-class BinarySearch { 
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+class Exercise_1 { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+    	while (l < r) {
+    		int m = l + (r-l)/2;
+    		if (arr[m] == x) return m;
+    		else if (arr[m] < x) l = m + 1;
+    		else r = m - 1;
+    	}
+    	
+    	return -1;
     } 
   
     // Driver method to test above 
     public static void main(String args[]) 
     { 
-        BinarySearch ob = new BinarySearch(); 
+        Exercise_1 ob = new Exercise_1(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
         int x = 10; 
